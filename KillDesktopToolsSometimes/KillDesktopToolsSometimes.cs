aa6a947fce360e699dfd9413c11184023abc4a1c
eia485 <eia485git@gmail.com>
2021-08-22 09:06:20 -0700

    Add project files.

---
+++ KillDesktopToolsSometimes/KillDesktopToolsSometimes.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System.Reflection;
+using System.Threading.Tasks;
+using FrooxEngine;
+
+
+namespace KillDesktopToolsSometimes
+{
+    public class KillDesktopToolsSometimes : NeosMod
+    {
+        public override string Name => "KillDesktopToolsSometimes";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.KillDesktopToolsSometimes");
+            Debug(Assembly.GetExecutingAssembly().FullName);
+            harmony.PatchAll();
+
+        }
+		
+		[HarmonyPatch(typeof(CommonTool), "SpawnAndEquip")]
+        class KillDesktopToolsSometimesPatch
+        {
+            public static bool Prefix(ref Task __result, CommonTool __instance)
+            {
+                if (__instance?.ActiveToolTip?.GetType() == typeof(TooltipMultiplexer))
+                {
+                    __result = new TaskCompletionSource<bool>().Task;
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+                
+
+            }
+        
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# KillDesktopToolsSometimes
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that disables the desktop tool keybinds only when a tooltip multiplexer is equipped. This is a temporary mod till [NeosModLoader](https://github.com/zkxs/NeosModLoader) implements a config system. once [NeosModLoader](https://github.com/zkxs/NeosModLoader) implements a config system this mod will be merged into [NeosKillDesktopTools](https://github.com/eia485/NeosKillDesktopTools/)
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [KillDesktopToolsSometimes.dll](https://github.com/eia485/NeosKillDesktopToolsSometimes/releases/latest/download/KillDesktopToolsSometimes.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+
