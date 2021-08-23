1cad3755457cda2d3b8f7a7a2e3aa17ff12c0cdc
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-08-23 07:00:12 -0700

    add link

--- KillDesktopToolsSometimes/KillDesktopToolsSometimes.cs
+++ KillDesktopToolsSometimes/KillDesktopToolsSometimes.cs
@@ -12,7 +12,7 @@ namespace KillDesktopToolsSometimes
         public override string Name => "KillDesktopToolsSometimes";
         public override string Author => "eia485";
         public override string Version => "1.0.0";
-        public override string Link => "";
+        public override string Link => "https://github.com/EIA485/NeosKillDesktopToolsSometimes/";
         public override void OnEngineInit()
         {
             Harmony harmony = new Harmony("net.eia485.KillDesktopToolsSometimes");
